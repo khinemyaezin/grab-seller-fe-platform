2449a36b4b0afd046d0675fa0503e1cc70bc9571
khinemyaezin <khinemyaezin@icloud.com>
2026-06-24 21:22:17 +0700

    Revert "chore: migrate version management to changesets and add auth event type definitions"
    This reverts commit 2e250249aaa5cd05389b992ac6f61009b0ad5f55.
    
    rollback

--- packages/seller-contracts/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @khinemyaezin/seller-contracts
-
-## 0.1.3
-
-### Patch Changes
-
-- add event contract for login/logout
--- packages/seller-contracts/src/events/index.ts
+++
@@ -1,4 +0,0 @@
-export type EventPayloads = {
-  'auth:login-success:v1': { userId: string; role: string };
-  'auth:logout:v1': undefined;
-};
